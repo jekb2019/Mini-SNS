52763da878445c9fad51fb32ee11cd7d8aea4a28
Jason Ko <jekb2019@gmail.com>
2021-09-15 15:57:40 +1200

    Use useInput for signup  modal to sotre id/email/pw

--- src/components/modalForms/signupForm/SignupForm.jsx
+++ src/components/modalForms/signupForm/SignupForm.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
 import '../../../common/styles/modalForm.css';
-const SignupForm = (props) => (
-  <div>
-    <form className="form">
-      <h1 className="title">Sign Up</h1>
-      <div className="inputSection">
-        <label className="label">Username</label>
-        <input type="text" id="username" className="input" required />
-      </div>
-      <div className="inputSection">
-        <label className="label">Email</label>
-        <input type="text" id="email" className="input" required />
-      </div>
-      <div className="inputSection">
-        <label className="label">Password</label>
-        <input type="text" id="password" className="input" required />
-      </div>
-      <button className="button">Go</button>
-    </form>
-  </div>
-);
+import useInput from '../../../util/useInput';
+const SignupForm = (props) => {
+  const [username, setUsernameByEvent, setUsernameByValue] = useInput('');
+  const [email, setEmailByEvent, setEmailByValue] = useInput('');
+  const [password, setPasswordByEvent, setPasswordByValue] = useInput('');
+
+  return (
+    <div>
+      <form className="form">
+        <h1 className="title">Sign Up</h1>
+        <div className="inputSection">
+          <label className="label">Username</label>
+          <input
+            value={username}
+            onChange={setUsernameByEvent}
+            type="text"
+            id="username"
+            className="input"
+            required
+          />
+        </div>
+        <div className="inputSection">
+          <label className="label">Email</label>
+          <input
+            value={email}
+            onChange={setEmailByEvent}
+            type="email"
+            id="email"
+            className="input"
+            required
+          />
+        </div>
+        <div className="inputSection">
+          <label className="label">Password</label>
+          <input
+            value={password}
+            onChange={setPasswordByEvent}
+            type="password"
+            id="password"
+            className="input"
+            required
+          />
+        </div>
+        <button className="button">Go</button>
+      </form>
+    </div>
+  );
+};
 
 export default SignupForm;

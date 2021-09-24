22ae76c5aaa9f201895ef5a25c05ac1e85043f0e
Jason Ko <jekb2019@gmail.com>
2021-09-24 14:08:18 +1200

    fix: move all intial set up to index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import styles from './App.module.css';
 import { useEffect, useState } from 'react';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
 import { filterCognitoUser } from './util/auth';
 import Forum from './pages/forum/Forum';
 import UserUIWrapper from './components/userUIWrapper/UserUIWrapper';
-
-Amplify.configure(awsconfig);
 
 function App({ postService, userService }) {
   const [user, setUser] = useState(null);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import PostService from './service/postService/postService';
 import UserService from './service/userService/userService';
 
+Amplify.configure(awsconfig);
+
 const userService = new UserService();
 const postService = new PostService();
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <App postService={postService} userService={userService} />,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App postService={postService} userService={userService} />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
